6caf616de9d17875d401d2183509912c5243f8bb
Peter G. (nephros) <sailfish@nephros.org>
2025-10-23 15:06:44 +0200

    Remove path from program

--- src/runner.cpp
+++ src/runner.cpp
@@ -76,7 +76,7 @@ Runner::Runner(QString program, QStringList /*runner_options*/, QStringList prog
   // launched app. This issue appeared after Qt 5.15.9 update
   env.remove("QT_WAYLAND_RESIZE_AFTER_SWAP");
 
-  QString tempTemplate(QStringLiteral("%1/qt-runner_%2_XXXXXX").arg(QDir::tempPath()).arg(program));
+  QString tempTemplate(QStringLiteral("%1/qt-runner_%2_XXXXXX").arg(QDir::tempPath()).arg(program.section('/', -1)));
   QTemporaryFile *qqc_env = new QTemporaryFile(tempTemplate);
   if(qqc_env->open()) {
       QTextStream q(qqc_env);
